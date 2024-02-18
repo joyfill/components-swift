fbda2822d01a2831db35b95a55c601f6315fecf5
Vishnu <ivishbishnoi@gmail.com>
2024-02-18 17:43:30 +0530

    Initial Commit

---
+++ Joyfill/JoyfillApp.swift
@@ -0,0 +1,17 @@
+//
+//  JoyfillApp.swift
+//  Joyfill
+//
+//  Created by ianmol's Macbook on 18/02/24.
+//
+
+import SwiftUI
+
+@main
+struct JoyfillApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
