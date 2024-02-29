e7667688038d9852c7b05d94519da846b59b2001
Vishnu <ivishbishnoi@gmail.com>
2024-02-29 12:45:11 +0530

    Fix signature image snapshot

--- JoyfillUI/Sources/JoyfillUI/View/Fields/SignatureView.swift
+++ JoyfillUI/Sources/JoyfillUI/View/Fields/SignatureView.swift
@@ -185,6 +185,7 @@ struct CanvasSignatureView: View {
                 Button(action: {
                     DispatchQueue.main.async {
                         signatureImage = CanvasView(lines: $lines)
+                            .frame(width: screenWidth, height: 220)
                              .snapshot()
                     }
                     presentationMode.wrappedValue.dismiss()
@@ -194,7 +195,6 @@ struct CanvasSignatureView: View {
                 .buttonStyle(.borderedProminent)
                 Spacer()
             }
-            
             Spacer()
         }
         .padding(.horizontal, 16.0)
@@ -202,44 +202,19 @@ struct CanvasSignatureView: View {
         
     }
 }
-// UIImage Extension
-//extension UIImage {
-//    func rotate(radians: CGFloat) -> UIImage {
-//        let rotatedSize = CGRect(origin: .zero, size: size)
-//            .applying(CGAffineTransform(rotationAngle: radians))
-//            .integral.size
-//        UIGraphicsBeginImageContext(rotatedSize)
-//        if let context = UIGraphicsGetCurrentContext() {
-//            let origin = CGPoint(x: rotatedSize.width / 2, y: rotatedSize.height / 2)
-//            context.translateBy(x: origin.x, y: origin.y)
-//            context.rotate(by: radians)
-//            draw(in: CGRect(x: -origin.y, y: -origin.x, width: size.width, height: size.height))
-//            let rotatedImage = UIGraphicsGetImageFromCurrentImageContext()
-//            UIGraphicsEndImageContext()
-//            return rotatedImage ?? self
-//        }
-//        return self
-//    }
-//    
-//    convenience init(view: any View) {
-//        UIGraphicsBeginImageContext(view.frame())
-//        view.layer.render(in: UIGraphicsGetCurrentContext()!)
-//        let image = UIGraphicsGetImageFromCurrentImageContext()
-//        UIGraphicsEndImageContext()
-//        self.init(cgImage: (image?.cgImage)!)
-//    }
-//}
 extension View {
     func snapshot() -> UIImage {
         let controller = UIHostingController(rootView: self)
-        
         let view = controller.view
-        
-        let renderer = UIGraphicsImageRenderer(size: view!.bounds.size)
-        let image = renderer.image { _ in
-            view?.drawHierarchy(in: view!.bounds, afterScreenUpdates: true)
+
+        let targetSize = controller.view.intrinsicContentSize
+        view?.bounds = CGRect(origin: .zero, size: targetSize)
+        view?.backgroundColor = .clear
+
+        let renderer = UIGraphicsImageRenderer(size: targetSize)
+
+        return renderer.image { _ in
+            view?.drawHierarchy(in: controller.view.bounds, afterScreenUpdates: true)
         }
-        
-        return image
     }
 }
