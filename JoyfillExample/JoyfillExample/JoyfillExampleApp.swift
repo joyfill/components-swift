fe914b3e20b08dbfd4ea627760ab3a90c81dffc8
Jeremy Pagley <jeremyjkp@gmail.com>
2024-03-14 14:25:55 -0400

    Work for release fix v2

--- JoyfillExample/JoyfillExample/JoyfillExampleApp.swift
+++ JoyfillExample/JoyfillExample/JoyfillExampleApp.swift
@@ -12,7 +12,7 @@ import JoyfillAPIService
 struct JoyfillExampleApp: App {
     init() {
         JoyfillAPIService.initialize(
-            userAccessToken: "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ0b2tlbiI6IjY1Yzc2NDI5ZGQ5NjIwNmM3ZTA3ZWQ5YiJ9.OhI3aY3na-3f1WWND8y9zU8xXo4R0SIUSR2BLB3vbsk",
+            userAccessToken: "",
             baseURL: "https://api-joy.joyfill.io/v1")
     }
     
