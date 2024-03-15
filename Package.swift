750a9ea8658a4bd23fe68076043aecf7d471a8e2
VishnuDuttBishnoi <159915932+VishnuDuttBishnoi@users.noreply.github.com>
2024-03-15 12:27:58 +0530

    Fix error

--- Package.swift
+++ Package.swift
@@ -11,7 +11,7 @@ let package = Package(
             targets: ["JoyfillAPIService"]),
     ],
     dependencies: [
-        .package(path: "../JoyfillModel"),
+        .package(path: "./JoyfillModel"),
     ],
     targets: [
         .target(
