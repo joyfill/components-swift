e1eb1283bebbe9a818fd32e62fbaff833a498c9a
VishnuDuttBishnoi <159915932+VishnuDuttBishnoi@users.noreply.github.com>
2024-03-15 23:22:57 +0530

    Add version

--- Package.swift
+++ Package.swift
@@ -14,8 +14,8 @@ let package = Package(
             targets: ["Joyfill"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/joyfill/JoyfillModel", branch: "main"),
-        .package(url: "https://github.com/joyfill/JoyfillAPIService", branch: "main"),
+        .package(url: "https://github.com/joyfill/JoyfillModel", exact: "0.2.1"),
+        .package(url: "https://github.com/joyfill/JoyfillAPIService",  exact: "0.2.1"),
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
