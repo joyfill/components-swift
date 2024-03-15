a72c1f3118cd020f67273202d0d6617dc8be798e
VishnuDuttBishnoi <159915932+VishnuDuttBishnoi@users.noreply.github.com>
2024-03-15 11:11:56 +0530

    Refactor package.swift

--- Package.swift
+++ Package.swift
@@ -1,30 +1,25 @@
 // swift-tools-version: 5.9
 // The swift-tools-version declares the minimum version of Swift required to build this package.
-
 import PackageDescription
 
 let package = Package(
     name: "JoyfillUI",
-    platforms: [
-        .iOS(.v15) // Set the minimum deployment target to iOS 15
-    ], products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
+    platforms: [.iOS(.v15)],
+    products: [
         .library(
             name: "JoyfillUI",
             targets: ["JoyfillUI"]),
     ],
     dependencies: [
-        .package(path: "JoyfillModel"),
-        .package(path: "JoyfillAPIService"),
+        .package(path: "./JoyfillAPIService"),
+        .package(path: "./JoyfillModel"),
     ],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
         .target(
             name: "JoyfillUI",
             dependencies: [
-                "JoyfillModel",
-                "JoyfillAPIService"
+                "JoyfillAPIService",
+                "JoyfillModel"
             ]
         ),
         .testTarget(
