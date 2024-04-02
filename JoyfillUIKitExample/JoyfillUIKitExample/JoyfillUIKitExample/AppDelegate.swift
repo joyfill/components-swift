1a9b211ebbd99d32d755450c2ecce8b917bc91e5
Vishnu <ivishbishnoi@gmail.com>
2024-04-02 14:20:31 +0530

    Add FormContainerViewController

--- JoyfillUIKitExample/JoyfillUIKitExample/JoyfillUIKitExample/AppDelegate.swift
+++ JoyfillUIKitExample/JoyfillUIKitExample/JoyfillUIKitExample/AppDelegate.swift
@@ -6,14 +6,15 @@
 //
 
 import UIKit
+import JoyfillAPIService
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-
-
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        JoyfillAPIService.initialize(
+            userAccessToken: "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ0b2tlbiI6IjY1Yzc2NDI5ZGQ5NjIwNmM3ZTA3ZWQ5YiJ9.OhI3aY3na-3f1WWND8y9zU8xXo4R0SIUSR2BLB3vbsk",
+            baseURL: "https://api-joy.joyfill.io/v1")
         return true
     }
 
