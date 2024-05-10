8567b9fcdf9f627b05f3e3e1be3a74eb673d993c
Vishnu <vishnubishnoi7@gmail.com>
2024-05-10 16:56:05 +0530

    Move table test cases to a seperate file

--- JoyfillExample/JoyfillUITests/JoyfillUITests.swift
+++ JoyfillExample/JoyfillUITests/JoyfillUITests.swift
@@ -48,7 +48,6 @@ final class JoyfillUITests: JoyfillUITestsBaseClass {
         XCTAssertGreaterThan(dropdownOptions.count, 0)
         firstOption = dropdownOptions.element(boundBy: 1)
         firstOption.tap()
-        XCTAssertFalse(app.sheets.firstMatch.exists)
         XCTAssertEqual("", onChangeResultValue().text!)
     }
 
@@ -77,92 +76,6 @@ final class JoyfillUITests: JoyfillUITestsBaseClass {
         app.swipeUp()
         app.buttons["SignatureIdentifier"].tap()
         app.navigationBars.buttons.element(boundBy: 0).tap()
-    }
-    
-    func goToTableDetailPage() {
-        app.swipeUp()
-        app.swipeUp()
-        app.swipeUp()
-        app.buttons["TableDetailViewIdentifier"].tap()
-    }
-    
-    func testTableTextFields() throws {
-        goToTableDetailPage()
-        
-        let firstTableTextField = app.textViews.matching(identifier: "TabelTextFieldIdentifier").element(boundBy: 0)
-        firstTableTextField.tap()
-        firstTableTextField.typeText("First")
-        
-        let secondTableTextField = app.textViews.matching(identifier: "TabelTextFieldIdentifier").element(boundBy: 1)
-        secondTableTextField.tap()
-        secondTableTextField.typeText("Second")
-        
-        let thirdTableTextField = app.textViews.matching(identifier: "TabelTextFieldIdentifier").element(boundBy: 2)
-        thirdTableTextField.tap()
-        thirdTableTextField.typeText("Third")
-        
-        app.navigationBars.buttons.element(boundBy: 0).tap()
-    }
-    
-    func testTableDropdownOption() throws {
-        goToTableDetailPage()
-        let dropdownButtons = app.buttons.matching(identifier: "TableDropdownIdentifier")
-        let firstdropdownButton = dropdownButtons.element(boundBy: 0)
-        firstdropdownButton.tap()
-        let dropdownOptions = app.buttons.matching(identifier: "TableDropdownOptionsIdentifier")
-        XCTAssertGreaterThan(dropdownOptions.count, 0)
-        let firstOption = dropdownOptions.element(boundBy: 1)
-        firstOption.tap()
-        app.navigationBars.buttons.element(boundBy: 0).tap()
-    }
-    
-    func testTableUploadImage() throws {
-        goToTableDetailPage()
-        let imageButtons = app.buttons.matching(identifier: "TableImageIdentifier")
-        let firstImageButton = imageButtons.element(boundBy: 0)
-        firstImageButton.tap()
-        app.buttons["ImageUploadImageIdentifier"].tap()
-        
-        let bottomCoordinate = app.windows.firstMatch.coordinate(withNormalizedOffset: CGVector(dx: 0.5, dy: 0.8))
-        let topCoordinate = app.windows.firstMatch.coordinate(withNormalizedOffset: CGVector(dx: 0.5, dy: 0.2))
-        topCoordinate.press(forDuration: 0, thenDragTo: bottomCoordinate)
-        app.navigationBars.buttons.element(boundBy: 0).tap()
-    }
-    
-    func testTabelDeleteImage() throws {
-        goToTableDetailPage()
-        let imageButtons = app.buttons.matching(identifier: "TableImageIdentifier")
-        let firstImageButton = imageButtons.element(boundBy: 0)
-        firstImageButton.tap()
-        XCUIApplication().scrollViews.children(matching: .other).element(boundBy: 0).children(matching: .other).element.children(matching: .image).matching(identifier: "DetailPageImageSelectionIdentifier").element(boundBy: 0).tap()
-        app.buttons["ImageDeleteIdentifier"].tap()
-        let bottomCoordinate = app.windows.firstMatch.coordinate(withNormalizedOffset: CGVector(dx: 0.5, dy: 0.8))
-        let topCoordinate = app.windows.firstMatch.coordinate(withNormalizedOffset: CGVector(dx: 0.5, dy: 0.2))
-        topCoordinate.press(forDuration: 0, thenDragTo: bottomCoordinate)
-        app.navigationBars.buttons.element(boundBy: 0).tap()
-    }
-    
-    func testTableAddRow() throws {
-        goToTableDetailPage()
-        app.buttons["TableAddRowIdentifier"].tap()
-        app.navigationBars.buttons.element(boundBy: 0).tap()
-        let value = try XCTUnwrap(onChangeResultChange().dictionary as? [String: Any])
-        let lastIndex = try Int(XCTUnwrap(value["targetRowIndex"] as? Double))
-        let newRow = try XCTUnwrap(value["row"] as? [String: Any])
-        XCTAssertNotNil(newRow["_id"])
-        XCTAssertEqual(3, lastIndex)
-    }
-    
-    func testTableDeleteRow() throws {
-        goToTableDetailPage()
-        app.scrollViews.otherElements.containing(.image, identifier:"MyButton").children(matching: .image).matching(identifier: "MyButton").element(boundBy: 2).tap()
-        app.buttons["TableDeleteRowIdentifier"].tap()
-        app.navigationBars.buttons.element(boundBy: 0).tap()
-        sleep(2)
-        let valueElements = try XCTUnwrap(onChangeResultValue().valueElements)
-        let lastRow = try XCTUnwrap(valueElements.last)
-        XCTAssertTrue(lastRow.deleted!)
-        XCTAssertEqual(3, valueElements.count)
     }
 }
 
