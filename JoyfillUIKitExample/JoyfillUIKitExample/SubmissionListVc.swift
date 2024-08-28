91b47d84397ae54e097e3caefbe4ef0919b4d99e
Vishnu <vishnubishnoi7@gmail.com>
2024-08-28 11:45:17 +0530

    Add storyboard and SubmissionListVC controller

---
+++ JoyfillUIKitExample/JoyfillUIKitExample/SubmissionListVc.swift
@@ -0,0 +1,23 @@
+//
+//  SubmissionListVc.swift
+//  JoyfillUIKitExample
+//
+//  Created by Babblu Bhaiya on 28/08/24.
+//
+
+import UIKit
+
+class SubmissionListVc: UIViewController {
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    var strings: [String] = ["Vikki", "Anmol", "jax", "Amit"]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+       
+    }
+    
+    
+
+}
