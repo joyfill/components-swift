aedd69e93ba64f54f3318d9337f275ff42b949c9
Vishnu <vishnubishnoi7@gmail.com>
2024-09-03 15:44:05 +0530

    Fix image detail not shown in table detail

--- Sources/JoyfillUI/View/Fields/TableView/TableImageView.swift
+++ Sources/JoyfillUI/View/Fields/TableView/TableImageView.swift
@@ -4,18 +4,20 @@ import SwiftUI
 import JoyfillModel
 
  struct TableImageView: View {
-     @State private var showMoreImages: Bool = false
+     @State var showMoreImages: Int = 5
+     @State var showMoreImages2: Bool = false
      private var cellModel: TableCellModel
      @State private var valueElements: [ValueElement] = []
      @State var showToast: Bool = false
 
      public init(cellModel: TableCellModel) {
          self.cellModel = cellModel
+         _showMoreImages = State(wrappedValue: 6)
      }
     
     var body: some View {
         Button(action: {
-            showMoreImages = true
+            showMoreImages = Int.random(in: 0...100)
         }, label: {
             HStack(spacing: 2) {
                 Image(systemName: "photo")
@@ -29,9 +31,12 @@ import JoyfillModel
         .onAppear {
             valueElements = cellModel.data.images ?? []
         }
-        .sheet(isPresented: $showMoreImages) {
+        .sheet(isPresented: $showMoreImages2) {
             MoreImageView(valueElements: $valueElements, isMultiEnabled: true, showToast: $showToast, uploadAction: uploadAction, isUploadHidden: false)
                 .disabled(cellModel.editMode == .readonly)
+        }
+        .onChange(of: showMoreImages) { newValue in
+            showMoreImages2 = true
         }
         .onChange(of: valueElements) { newValue in
             var editedCell = cellModel.data
