1c60187e5b30910a4c63d35d97c1e825b02d09dc
Vivek kumar <vivekjyani11@gmail.com>
2025-03-03 19:03:16 +0530

    Fix build error

--- JoyfillSwiftUIExample/JoyfillExample/DocumentSubmissionsListView.swift
+++ JoyfillSwiftUIExample/JoyfillExample/DocumentSubmissionsListView.swift
@@ -90,125 +90,125 @@ struct DocumentSubmissionsListView: View {
                 }
             }
         }
-        
-        private var pageID: String {
-            return ""
+    }
+    
+    private var pageID: String {
+        return ""
+    }
+    
+    private var changeManager: ChangeManager {
+        ChangeManager(apiService: apiService, showImagePicker: showImagePicker, showScan: showScan)
+    }
+    
+    private func showImagePicker(uploadHandler: ([String]) -> Void) {
+        uploadHandler(["https://example.com/sample-image"])
+    }
+    
+    private func showScan(captureHandler: @escaping (ValueUnion) -> Void) {
+        currentCaptureHandler = captureHandler
+        showCameraScannerView = true
+        presentCameraScannerView()
+    }
+    
+    private func fetchLocalDocument() {
+        isloading = true
+        DispatchQueue.global().async {
+            self.document = sampleJSONDocument(fileName: "TableNewColumns")
+            DispatchQueue.main.async {
+                showDocumentDetails = true
+                isloading = false
+            }
         }
-        
-        private var changeManager: ChangeManager {
-            ChangeManager(apiService: apiService, showImagePicker: showImagePicker, showScan: showScan)
-        }
-        
-        private func showImagePicker(uploadHandler: ([String]) -> Void) {
-            uploadHandler(["https://example.com/sample-image"])
-        }
-        
-        private func showScan(captureHandler: @escaping (ValueUnion) -> Void) {
-            currentCaptureHandler = captureHandler
-            showCameraScannerView = true
-            presentCameraScannerView()
-        }
-        
-        private func fetchLocalDocument() {
-            isloading = true
-            DispatchQueue.global().async {
-                self.document = sampleJSONDocument(fileName: "TableNewColumns")
-                DispatchQueue.main.async {
-                    showDocumentDetails = true
-                    isloading = false
+    }
+    
+    private func fetchDocument(_ submission: Document) {
+        isloading = true
+        apiService.fetchJoyDoc(identifier: submission.identifier) { result in
+            DispatchQueue.main.async {
+                isloading = false
+                switch result {
+                case .success(let data):
+                    do {
+                        if let dictionary = try JSONSerialization.jsonObject(with: data, options: .mutableContainers) as? [String: Any] {
+                            self.document = JoyDoc(dictionary: dictionary)
+                            showDocumentDetails = true
+                        }
+                    } catch {
+                        print("Error decoding JSON: \(error)")
+                    }
+                case .failure(let error):
+                    print("error: \(error.localizedDescription)")
                 }
             }
         }
-        
-        private func fetchDocument(_ submission: Document) {
-            isloading = true
-            apiService.fetchJoyDoc(identifier: submission.identifier) { result in
-                DispatchQueue.main.async {
-                    isloading = false
-                    switch result {
-                    case .success(let data):
-                        do {
-                            if let dictionary = try JSONSerialization.jsonObject(with: data, options: .mutableContainers) as? [String: Any] {
-                                self.document = JoyDoc(dictionary: dictionary)
-                                showDocumentDetails = true
-                            }
-                        } catch {
-                            print("Error decoding JSON: \(error)")
-                        }
-                    case .failure(let error):
-                        print("error: \(error.localizedDescription)")
+    }
+    
+    func presentCameraScannerView() {
+        guard let topVC = UIViewController.topViewController() else {
+            print("No top view controller found.")
+            return
+        }
+        let hostingController: UIHostingController<AnyView>
+        if #available(iOS 16.0, *) {
+            let swiftUIView = CameraScanner(
+                startScanning: $showCameraScannerView,
+                scanResult: $scanResults,
+                onSave: { result in
+                    if let currentCaptureHandler = currentCaptureHandler {
+                        currentCaptureHandler(.string(result))
                     }
+                }
+            )
+            hostingController = UIHostingController(rootView: AnyView(swiftUIView))
+        } else {
+            // Fallback on earlier versions
+            let fallbackView = Text("Camera scanner is not available on this version.")
+                .padding()
+                .multilineTextAlignment(.center)
+            hostingController = UIHostingController(rootView: AnyView(fallbackView))
+        }
+        
+        topVC.present(hostingController, animated: true, completion: nil)
+    }
+    
+    private func fetchDocuments(identifier: String, completion: @escaping (() -> Void)) {
+        apiService.fetchDocuments(identifier: identifier, page: 1, limit: 20) { result in
+            DispatchQueue.main.async {
+                self.fetchSubmissions = false
+                self.isloading = false
+                switch result {
+                case .success(let documents):
+                    self.documents = documents
+                    completion()
+                case .failure(let error):
+                    print("Error fetching documents: \(error.localizedDescription)")
                 }
             }
         }
+    }
+    
+    func fetchData() {
+        fetchDocuments(identifier: identifier){}
+    }
+    
+    private func loadMoreDocuments() {
+        guard !isLoadingMoreDocuments, hasMoreDocuments else { return }
+        isLoadingMoreDocuments = true
+        let nextPage = currentDocumentPage + 1
         
-        func presentCameraScannerView() {
-            guard let topVC = UIViewController.topViewController() else {
-                print("No top view controller found.")
-                return
-            }
-            let hostingController: UIHostingController<AnyView>
-            if #available(iOS 16.0, *) {
-                let swiftUIView = CameraScanner(
-                    startScanning: $showCameraScannerView,
-                    scanResult: $scanResults,
-                    onSave: { result in
-                        if let currentCaptureHandler = currentCaptureHandler {
-                            currentCaptureHandler(.string(result))
-                        }
+        apiService.fetchDocuments(identifier: identifier, page: nextPage, limit: 20) { result in
+            DispatchQueue.main.async {
+                isLoadingMoreDocuments = false
+                switch result {
+                case .success(let newDocuments):
+                    if newDocuments.isEmpty {
+                        hasMoreDocuments = false
+                    } else {
+                        documents.append(contentsOf: newDocuments)
+                        currentDocumentPage = nextPage
                     }
-                )
-                hostingController = UIHostingController(rootView: AnyView(swiftUIView))
-            } else {
-                // Fallback on earlier versions
-                let fallbackView = Text("Camera scanner is not available on this version.")
-                    .padding()
-                    .multilineTextAlignment(.center)
-                hostingController = UIHostingController(rootView: AnyView(fallbackView))
-            }
-            
-            topVC.present(hostingController, animated: true, completion: nil)
-        }
-        
-        private func fetchDocuments(identifier: String, completion: @escaping (() -> Void)) {
-            apiService.fetchDocuments(identifier: identifier, page: 1, limit: 20) { result in
-                DispatchQueue.main.async {
-                    self.fetchSubmissions = false
-                    self.isloading = false
-                    switch result {
-                    case .success(let documents):
-                        self.documents = documents
-                        completion()
-                    case .failure(let error):
-                        print("Error fetching documents: \(error.localizedDescription)")
-                    }
-                }
-            }
-        }
-        
-        func fetchData() {
-            fetchDocuments(identifier: identifier){}
-        }
-        
-        private func loadMoreDocuments() {
-            guard !isLoadingMoreDocuments, hasMoreDocuments else { return }
-            isLoadingMoreDocuments = true
-            let nextPage = currentDocumentPage + 1
-            
-            apiService.fetchDocuments(identifier: identifier, page: nextPage, limit: 20) { result in
-                DispatchQueue.main.async {
-                    isLoadingMoreDocuments = false
-                    switch result {
-                    case .success(let newDocuments):
-                        if newDocuments.isEmpty {
-                            hasMoreDocuments = false
-                        } else {
-                            documents.append(contentsOf: newDocuments)
-                            currentDocumentPage = nextPage
-                        }
-                    case .failure(let error):
-                        print("Error loading more templates: \(error.localizedDescription)")
-                    }
+                case .failure(let error):
+                    print("Error loading more templates: \(error.localizedDescription)")
                 }
             }
         }
