bbf1a29eae0d88bb17392009680dd9db8bb277a3
Vishnu Dutt <vishnubishnoi7@gmail.com>
2025-04-22 23:04:22 +0530

    MultiLineTextView working

--- JoyfillSwiftUIExample/JoyfillExample/LiveViewTest.swift
+++ JoyfillSwiftUIExample/JoyfillExample/LiveViewTest.swift
@@ -17,13 +17,13 @@ struct LiveViewTest: View {
     init() {
         let document = JoyDoc
             .addDocument()
-            .addNumberField(identifier: "num1", formula: "{num3} + {num2}")
-            .addNumberField(identifier: "num2", formula: "{num4} + 5")
-            .addNumberField(identifier: "num3", value: 500)
+//            .addNumberField(identifier: "num1", formula: "{num3} + {num2}")
+//            .addNumberField(identifier: "num2", formula: "{num4} + 5")
+//            .addNumberField(identifier: "num3", value: 500)
             .addNumberField(identifier: "num4", value: 133)
-            .addTextField(identifier: "text1", formula: "if({num4} > 3, \"yes\", \"no\")", value: "Hello, World!")
-            .addDateField(identifier: "due_date", formula: "", date: Date())
-
+//            .addTextField(identifier: "text1", formula: "if({num4} > 3, \"yes\", \"no\")", value: "Hello, World!")
+//            .addDateField(identifier: "due_date", formula: "", date: Date())
+            .addTextareaField(formula: "if({num4} > 3, \"yes\", \"no\")")
 
         self.documentEditor = DocumentEditor(document: document)
     }
