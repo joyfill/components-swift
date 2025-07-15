c3c671bb44309ff5ff1d2697e5b858a4378e27f9
Vishnu Dutt <vishnubishnoi7@gmail.com>
2025-07-15 21:17:32 +0530

    Update SchemaValidationExampleView.swift

--- JoyfillSwiftUIExample/JoyfillExample/SchemaValidationExampleView.swift
+++ JoyfillSwiftUIExample/JoyfillExample/SchemaValidationExampleView.swift
@@ -4,12 +4,12 @@ import JSONSchema
 import JoyfillModel
 import Foundation
 
-struct SchemaValidationExampleView: View, FormChangeEvent {
+struct SchemaValidationExampleView: View {
     init() {
         validate()
     }
-    
-    
+
+
     public var body: some View {
         NavigationView {
             Text("sadas")
@@ -17,29 +17,21 @@ struct SchemaValidationExampleView: View, FormChangeEvent {
     }
 
     // MARK: - Validate Document
-        func validate() {
-            do {
-                let result = try JSONSchema.validate(sampleJSONDocument(fileName: "ErrorHandling").dictionary, schema: sampleJSONDocument(fileName: "joyfill-schema 2").dictionary)
-                if result.valid {
-                    print("🔸 JSON is valid! 🌍")
-                    return
-                } else {
-                    for error in result.errors ?? [] {
-                        let instanceLoc = error.instanceLocation.path
-                        let keywordLoc = error.keywordLocation.path
-                        print("🔸 \(error.description)\n📍 Instance: \(instanceLoc)\n🧩 Keyword: \(keywordLoc)\n🌍")
-                    }
+    func validate() {
+        do {
+            let result = try JSONSchema.validate(sampleJSONDocument(fileName: "ErrorHandling").dictionary, schema: sampleJSONDocument(fileName: "joyfill-schema 2").dictionary)
+            if result.valid {
+                print("🔸 JSON is valid! 🌍")
+                return
+            } else {
+                for error in result.errors ?? [] {
+                    let instanceLoc = error.instanceLocation.path
+                    let keywordLoc = error.keywordLocation.path
+                    print("🔸 \(error.description)\n📍 Instance: \(instanceLoc)\n🧩 Keyword: \(keywordLoc)\n🌍")
                 }
-            } catch {
-                 print("Validation failure: \(error.localizedDescription)")
             }
+        } catch {
+            print("Validation failure: \(error.localizedDescription)")
         }
-}
-
-extension SchemaValidationExampleView {
-    func onChange(changes: [JoyfillModel.Change], document: JoyfillModel.JoyDoc) {}
-    func onFocus(event: JoyfillModel.FieldIdentifier) { }
-    func onBlur(event: JoyfillModel.FieldIdentifier) {}
-    func onUpload(event: JoyfillModel.UploadEvent) {}
-    func onCapture(event: JoyfillModel.CaptureEvent) {}
+    }
 }
