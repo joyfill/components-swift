aedcb0a91da0a07827e6858e537b325db2d53ecc
Vishnu Dutt <vishnubishnoi7@gmail.com>
2025-07-17 20:44:01 +0530

    Cleanup

--- Sources/JoyfillUI/ViewModels/DocumentEditor.swift
+++ Sources/JoyfillUI/ViewModels/DocumentEditor.swift
@@ -29,7 +29,7 @@ public class DocumentEditor: ObservableObject {
     public var events: FormChangeEvent?
     
     private var validationHandler: ValidationHandler!
-    private var conditionalLogicHandler: ConditionalLogicHandler!
+    var conditionalLogicHandler: ConditionalLogicHandler!
     private var JoyfillDocContext: JoyfillDocContext!
 
     public init(document: JoyDoc, mode: Mode = .fill, events: FormChangeEvent? = nil, pageID: String? = nil, navigation: Bool = true, isPageDuplicateEnabled: Bool = false) {
@@ -643,42 +643,5 @@ extension DocumentEditor {
             }
             onChangeDuplicatePage(viewId: "", page: duplicatedPage, fields: document.fields, fileId: document.files[0].id ?? "", targetIndex: targetIdnex, newFields: newFields)
         }
-    }
-}
-
-extension DocumentEditor: JoyDocProvider {
-    func currentFieldIdentifier() -> String? {
-        nil
-    }
-
-    func formula(with id: String) -> JoyfillModel.Formula? {
-        document.formulas.first { $0.id == id }
-    }
-
-    func allFormulsFields() -> [JoyfillModel.JoyDocField] {
-        allFields.filter { $0.formulas != nil }
-    }
-
-    func setFieldHidden(_ hidden: Bool, for identifier: String) {
-        print("setFieldHidden >>>>>", hidden, identifier)
-        guard var field = allFields.first(where: { $0.id == identifier }) else {
-            return
-        }
-        conditionalLogicHandler.showFieldMap[field.id!] = hidden
-        refreshField(fieldId: field.id!)
-    }
-
-    func updateValue(for identifier: String, value: JoyfillModel.ValueUnion) {
-        print("updateValue called >>>>>>>", value)
-        guard var field = allFields.first(where: { $0.id == identifier }) else {
-            return
-        }
-        guard let fieldID = field.id else { return }
-        field.value = value
-        fieldMap[fieldID] = field
-        refreshField(fieldId: fieldID)
-        refreshDependent(for: fieldID)
-        // TODO: Neet to add pageid and cleanup here
-        handleFieldsOnChange(fieldIdentifier: FieldIdentifier(fieldID: field.id!, pageID: "", fileID: document.files.first?.id), currentField: field)
     }
 }
