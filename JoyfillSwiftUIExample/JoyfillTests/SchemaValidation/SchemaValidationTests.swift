3cd10320f771f7b34b1b6eb1a2760ce755bb5f44
Vivek kumar <vivekjyani11@gmail.com>
2025-07-28 13:52:23 +0530

    Add some version unit test for schema validation

--- JoyfillSwiftUIExample/JoyfillTests/SchemaValidation/SchemaValidationTests.swift
+++ JoyfillSwiftUIExample/JoyfillTests/SchemaValidation/SchemaValidationTests.swift
@@ -15,4 +15,73 @@ final class SchemaValidationTests: XCTestCase {
         DocumentEditor(document: document)
     }
     
+    //Version tests
+    // 1. Backward compatiblity, 2. Forward compatiblity, 3. Normal
+    func testForwardCompatality() {
+        let document = JoyDoc(dictionary: ["v": "2"])
+            .setFile()
+            .setMobileView()
+            .setPageFieldInMobileView()
+            .setPageField()
+                    
+        let schemaManager = JoyfillSchemaManager()
+        if let error = schemaManager.validateSchema(document: document) {
+            XCTAssertEqual("ERROR_SCHEMA_VERSION", error.code)
+        }
+        //check sdkversion and schema version
+    }
+    
+    func testVersion() {
+        let document = JoyDoc(dictionary: ["v": "1"])
+            .setFile()
+            .setMobileView()
+            .setPageFieldInMobileView()
+            .setPageField()
+                    
+        let schemaManager = JoyfillSchemaManager()
+        if let error = schemaManager.validateSchema(document: document) {
+            XCTAssertNotEqual("ERROR_SCHEMA_VERSION", error.code)
+            XCTAssertEqual("ERROR_SCHEMA_VALIDATION", error.code)
+        }
+    }
+    
+    func testVersionWithPoints() {
+        let document = JoyDoc(dictionary: ["v": "1.6.9"])
+            .setDocument()
+            .setFile()
+            .setPageWithFieldPosition()
+            .setHeadingText()
+            .setTextField()
+                    
+        let schemaManager = JoyfillSchemaManager()
+        let error = schemaManager.validateSchema(document: document)
+        XCTAssertTrue(error == nil)
+    }
+    
+    func testValidationWithAllValid() {
+        let document = JoyDoc()
+            .setDocument()
+            .setFile()
+            .setPageWithFieldPosition()
+            .setHeadingText()
+            .setTextField()
+                    
+        let schemaManager = JoyfillSchemaManager()
+        let error = schemaManager.validateSchema(document: document)
+        XCTAssertTrue(error == nil)
+    }
+    
+    func testValidationWithNilVersion() {
+        let document = JoyDoc()
+            .setFile()
+            .setMobileView()
+            .setPageFieldInMobileView()
+            .setPageField()
+                    
+        let schemaManager = JoyfillSchemaManager()
+        if let error = schemaManager.validateSchema(document: document) {
+            XCTAssertNotEqual("ERROR_SCHEMA_VERSION", error.code)
+            XCTAssertEqual("ERROR_SCHEMA_VALIDATION", error.code)
+        }
+    }
 }
