333e83f1e879eaa8349f00bd85ba85fd5982fb1b
Vivek kumar <vivekjyani11@gmail.com>
2025-07-28 14:11:44 +0530

    Add on error callback unit tests

--- JoyfillSwiftUIExample/JoyfillTests/SchemaValidation/SchemaValidationTests.swift
+++ JoyfillSwiftUIExample/JoyfillTests/SchemaValidation/SchemaValidationTests.swift
@@ -85,3 +85,64 @@ final class SchemaValidationTests: XCTestCase {
         }
     }
 }
+
+private class MockFormChangeEvent: FormChangeEvent {
+    var capturedError: JoyfillError?
+
+    func onChange(changes: [Change], document: JoyDoc) {}
+    func onFocus(event: FieldIdentifier) {}
+    func onBlur(event: FieldIdentifier) {}
+    func onUpload(event: UploadEvent) {}
+    func onCapture(event: CaptureEvent) {}
+    func onError(error: JoyfillError) { capturedError = error }
+}
+
+extension SchemaValidationTests {
+    // Ensures that the onError callback is triggered with the correct error when schema validation fails.
+    func testDocumentEditor_OnErrorCallbackIsTriggered() {
+        let invalidDoc = JoyDoc()
+            .setFile()
+            .setMobileView()
+            .setPageFieldInMobileView()
+            .setPageField()
+        let mockEvents = MockFormChangeEvent()
+
+        _ = DocumentEditor(document: invalidDoc, events: mockEvents) // validateSchema defaults to `true`
+
+        XCTAssertNotNil(mockEvents.capturedError, "onError should be called for invalid document")
+        if case .schemaValidationError(let schemaError) = mockEvents.capturedError! {
+            XCTAssertEqual(schemaError.code, "ERROR_SCHEMA_VALIDATION")
+        } else {
+            XCTFail("Expected schemaValidationError")
+        }
+    }
+
+    func testDocumentEditor_WithValidationDisabled_ShouldNotTriggerError() {
+        let invalidDoc = JoyDoc()
+            .setFile()
+            .setMobileView()
+            .setPageFieldInMobileView()
+            .setPageField()
+        let mockEvents = MockFormChangeEvent()
+
+        let editor = DocumentEditor(document: invalidDoc, events: mockEvents, validateSchema: false)
+
+        XCTAssertNil(mockEvents.capturedError, "onError should NOT be called when validation is disabled")
+        XCTAssertNil(editor.schemaError, "schemaError should be nil when validation is disabled")
+    }
+    
+    func testOnErrorWithValidDocument() {
+        let document = JoyDoc()
+            .setDocument()
+            .setFile()
+            .setPageWithFieldPosition()
+            .setHeadingText()
+            .setTextField()
+        
+        let mockEvents = MockFormChangeEvent()
+
+        let editor = DocumentEditor(document: document, events: mockEvents)
+        XCTAssertNil(mockEvents.capturedError, "onError should NOT be called when document is valid")
+        XCTAssertNil(editor.schemaError, "schemaError should be nil when document is valid")
+    }
+}
