42bc923d32197cdfcc8ecf2822a2f610ef31f036
Vivek kumar <vivekjyani11@gmail.com>
2025-07-28 12:59:26 +0530

    Add new file for schema validation tests

---
+++ JoyfillSwiftUIExample/JoyfillTests/SchemaValidation/SchemaValidationTests.swift
@@ -0,0 +1,18 @@
+//
+//  SchemaValidationtests.swift
+//  JoyfillExample
+//
+//  Created by Vivek on 28/07/25.
+//
+import XCTest
+import Foundation
+import SwiftUI
+import JoyfillModel
+import Joyfill
+
+final class SchemaValidationTests: XCTestCase {
+    func documentEditor(document: JoyDoc) -> DocumentEditor {
+        DocumentEditor(document: document)
+    }
+    
+}
