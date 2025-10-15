16d31d75245554ae4d59a159e8cd6f6fc7f5f8e0
Vishnu Dutt <vishnubishnoi7@gmail.com>
2025-10-15 11:21:32 +0530

    Clarify FormChangeEvent protocol docs

--- Sources/JoyfillUI/View/Types.swift
+++ Sources/JoyfillUI/View/Types.swift
@@ -226,51 +226,33 @@ public struct Change {
     }
 }
 
-/// `FormChangeEvent` is a protocol that defines methods for listening to form change events.
+/// Protocol adopted by host applications to observe Joyfill form events.
 public protocol FormChangeEvent {
 
-    /// Used to listen to any field change events.
-    ///
-    /// (changelogs: object_array, doc: object) => {}
-    ///
-    /// - changelogs: object_array :
-    ///   - Can contain one ore more of the changelog object types supported.
-    ///
-    /// - doc: object :
-    ///    - Fully updated JoyDoc JSON structure with changes applied.
+    /// Called whenever one or more fields change.
+    /// - Parameters:
+    ///   - changes: Collection of changelog entries describing the update.
+    ///   - document: Updated JoyDoc snapshot after the change was applied.
     func onChange(changes: [Change], document: JoyDoc)
 
-    /// Used to listen to field focus events.
-    ///
-    /// (params: object, e: object) => {}
-    ///
-    ///  params: object :
-    /// - Specifies information about the focused field.
-    ///
-    /// e: object :
-    ///  - Element helper methods.
-    ///  - blur: Function :
-    ///     - Triggers the field blur event for the focused field.
-    ///     - If there are pending changes in the field that have not triggered the `onChange` event yet then the `e.blur()` function will trigger both the change and blur events in the following order: 1) `onChange` 2) `onBlur`.
-    ///     - If the focused field utilizes a modal for field modification, ie. signature, image, tables, etc. the `e.blur()` will close the modal.
+    /// Called when a field gains focus.
+    /// - Parameter event: Identifier describing the field that gained focus.
     func onFocus(event: FieldIdentifier)
 
-    /// Used to listen to field focus events.
-    ///
-    ///  (params: object) => {}
-    ///
-    ///  params: object :
-    ///  - Specifies information about the blurred field.
+    /// Called when a field loses focus.
+    /// - Parameter event: Identifier describing the field that lost focus.
     func onBlur(event: FieldIdentifier)
 
-    /// Used to listen to file upload events.
-    ///
-    /// (params: object) => {} :
-    /// - Specifies information about the uploaded file.
+    /// Called when an upload operation completes.
+    /// - Parameter event: Payload describing the upload context.
     func onUpload(event:UploadEvent)
 
+    /// Called when a capture target (e.g. table cell, nested collection) requests user input.
+    /// - Parameter event: Payload describing the capture request and completion handler.
     func onCapture(event: CaptureEvent)
 
+    /// Called when a Joyfill UI error is encountered.
+    /// - Parameter error: Error describing what went wrong.
     func onError(error: JoyfillError)
 }
 
