69b7bc90801ec26f9bdd47963b40e8d4b189e78a
Vivek kumar <vivekjyani11@gmail.com>
2026-05-05 18:11:16 +0530

    Update schema version in tests

--- JoyfillSwiftUIExample/JoyfillTests/SchemaValidation/SchemaValidationTests.swift
+++ JoyfillSwiftUIExample/JoyfillTests/SchemaValidation/SchemaValidationTests.swift
@@ -12,7 +12,7 @@ import Joyfill
 
 final class SchemaValidationTests: XCTestCase {
     /// The schema version expected in `SchemaValidationError.details`.
-    private let expectedSchemaVersion = "1.0.0"
+    private let expectedSchemaVersion = "1.0.1"
     private let sdkVersion = "3.0.0-rc17"
     
     func documentEditor(document: JoyDoc) -> DocumentEditor {
