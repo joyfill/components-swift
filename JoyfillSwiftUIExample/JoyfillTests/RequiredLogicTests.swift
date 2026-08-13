bc5c57b72d99068a5e1f80344e8c2632398eae6e
Vivek kumar <vivekjyani11@gmail.com>
2026-08-13 12:22:29 +0530

    Comment for adding new test

--- JoyfillSwiftUIExample/JoyfillTests/RequiredLogicTests.swift
+++ JoyfillSwiftUIExample/JoyfillTests/RequiredLogicTests.swift
@@ -793,3 +793,9 @@ final class RequiredLogicTests: XCTestCase {
         XCTAssertTrue(editor.isFieldRequired(fieldID: collectionFieldID))
     }
 }
+
+//Missing tests
+//
+//External row updates
+//validate() method tests
+//page duplicate and delete tests with these functionalities
